758d763238bb6fa648b748048ba05436b202549a
mazeu <marciomazeu@hotmail.com>
2023-02-04 17:46:09 -0300

    add all archives

---
+++ squadmanager/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace squadmanager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ squadmanager/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace squadmanager.Models
+{
+    public class UserViewModel
+    {
+        private string Username { get; set; }
+
+        private string Email { get; set; }
+        private string Passsword { get; set; }
+    }
+}
